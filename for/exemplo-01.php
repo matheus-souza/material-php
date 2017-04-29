f36c60bddaf4d49ef7f9df58ad40c220819fbaf0
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-29 02:03:33 -0300

    Aula sobre for

---
+++ for/exemplo-01.php
@@ -0,0 +1,14 @@
+<?php 
+
+	for ($i=0; $i < 10; $i++) { 
+		echo $i."<br>";
+	}
+
+	for ($i=0; $i < 1000; $i+=5) { 
+		if ($i > 200 && $i < 800) continue;
+
+		if ($i === 900) break;
+		echo $i."<br>";
+	}
+
+ ?>
