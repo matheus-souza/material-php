33ce2a1ea8982e484d3bae0eb1aab5a8edda545c
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-29 02:03:46 -0300

    Aula sobre foreach

---
+++ foreach/exemplo-01.php
@@ -0,0 +1,15 @@
+<?php 
+
+	$meses = array(
+		"Janeiro", "Fevereiro", "Março",
+		"Abril", "Maio", "Junho",
+		"Julho", "Agosto", "Setembro",
+		"Outubro", "Novembro", "Dezembro");
+
+	foreach ($meses as $index => $mes) {
+
+		echo "O index é ".$index."<br>";
+		echo "O mês é: ".$mes."<br>";
+	}
+
+ ?>
