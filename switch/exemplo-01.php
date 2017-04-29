bb28f6fa91e97c00d8edecc37fe899c4677c0bae
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-29 02:04:23 -0300

    Aula sobre switch

---
+++ switch/exemplo-01.php
@@ -0,0 +1,36 @@
+<?php 
+
+	$diaSemana = date("w");
+
+	echo $diaSemana;
+
+	echo "<br>";
+
+	switch ($diaSemana) {
+		case 0:
+			echo "Domingo";
+			break;
+		case 1:
+			echo "Segunda-feira";
+			break;
+		case 2:
+			echo "Terça-feira";
+			break;
+		case 3:
+			echo "Quarta-feira";
+			break;
+		case 4:
+			echo "Quinta-feira";
+			break;
+		case 5:
+			echo "Sexta-feira";
+			break;
+		case 6:
+			echo "Sábado";
+			break;
+		default:
+			echo "Data inválida";
+			break;
+	}
+
+ ?>
