e28a7189d80c507af9af5b906434daf1143132a8
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-05-02 15:13:06 -0300

    Aula sobre classe MySQLi

---
+++ mysqli/exemplo-01.php
@@ -0,0 +1,23 @@
+<?php 
+
+	$conn = new mysqli("127.0.0.1", "root", "", "dbphp7");
+
+	if ($conn->connect_error) {
+		echo "Error: ".$conn->connect_error;
+	}
+
+	$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES(?,?)");
+
+	$stmt->bind_param("ss", $login, $senha);
+
+	$login = "user";
+	$senha = "12345";
+
+	$stmt->execute();
+
+	$login = "root";
+	$senha = "!@#$";
+
+	$stmt->execute();
+
+ ?>
