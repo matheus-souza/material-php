53aed36bf27b906555e52560f146b2ba819e52cf
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-05-02 18:31:23 -0300

    Corrigindo bugs

--- dao/class/Sql.php
+++ dao/class/Sql.php
@@ -9,7 +9,7 @@
 
 		private function setParams($statment, $parameters = array()) {
 			foreach ($parameters as $key => $value) {
-				$this->setParam($key, $value);
+				$this->setParam($statment, $key, $value);
 			}
 		}
 
