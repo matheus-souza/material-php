26ab0e1dce8d773b60ff20a6ff4a9b4ea14bf71b
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-05-01 23:15:24 -0300

    Aula sobre encapsulamento

---
+++ poo/encapsulamento/exemplo-01.php
@@ -0,0 +1,20 @@
+<?php 
+
+	class Pessoa {
+		public $nome = "Rasmus Lerdorf";
+		protected $idade = 48;
+		private $senha = "123456";
+
+		public function verDados() {
+			echo $this->nome."<br/>";
+			echo $this->idade."<br/>";
+			echo $this->senha."<br/>";
+		}
+	}
+
+	$objeto = new Pessoa();
+
+	//echo $objeto->nome;
+	$objeto->verDados();
+
+ ?>
