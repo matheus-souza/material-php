cafefe440b28a4eb81c91da918026f9a3362a60d
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-05-09 00:48:01 -0300

    Aula sobre upload de arquivos

---
+++ upload/exemplo-01.php
@@ -0,0 +1,31 @@
+<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="fileUpload">
+
+	<button type="submit">Enviar</button>
+</form>
+
+<?php 
+
+	if ($_SERVER["REQUEST_METHOD"] === "POST") {
+		$file = $_FILES["fileUpload"];
+
+		if ($file["error"]) {
+			throw new Exception("Error: ".$file["error"]);
+			
+		}
+
+		$dirUploads = "uploads";
+
+		if (!is_dir($dirUploads)) {
+			mkdir($dirUploads);
+		}
+
+		if (move_uploaded_file($file["tmp_name"], $dirUploads.DIRECTORY_SEPARATOR.$file["name"])) {
+			echo "Upload realizado com sucesso!";
+		} else {
+			throw new Exception("Não foi possivel fazer o upload");
+		}
+	}
+
+
+ ?>
