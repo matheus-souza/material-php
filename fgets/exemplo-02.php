2b41c87a6957aae0074e690e7b3293a81826f8f7
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-05-09 00:34:11 -0300

    Exemplo de renderização de imagem com base64

---
+++ fgets/exemplo-02.php
@@ -0,0 +1,17 @@
+<?php 
+
+	$filename = "images/test-250-250.png";
+
+	$base64 = base64_encode(file_get_contents($filename));
+
+	$fileinfo = new finfo(FILEINFO_MIME_TYPE);
+
+	$mimetype = $fileinfo->file($filename);
+
+	$base64encode = "data:".$mimetype.";base64,".$base64;
+
+ ?>
+
+ <a href="<?=$base64encode?>" target="_blank">Link para imagem</a>
+
+ <img src="<?=$base64encode?>">
