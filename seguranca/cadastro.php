22ff7c764433ac6ec076981bd1e064853f9bcda3
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-05-10 21:15:08 -0300

    Aula sobre reCAPTCHA

---
+++ seguranca/cadastro.php
@@ -0,0 +1,29 @@
+<?php 
+
+	$email = $_POST["inputEmail"];
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+		//chave disponibilizada no site
+		"secret" => "6LfT2yAUAAAAAEAF9eAbqgv5-MygpV2Kq-hCSavE",
+		"response" => $_POST["g-recaptcha-response"],
+		"remoteip" => $_SERVER["REMOTE_ADDR"]
+	)));
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$recaptcha = json_decode(curl_exec($ch), true);
+
+	curl_close($ch);
+
+	if ($recaptcha["success"] === true) {
+	 	echo "OK: ".$_POST["inputEmail"];
+	} else {
+		header("Location: exemplo-04.php");
+	}
+ ?>
---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,9 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<form action="cadastro.php" method="post">
+ 	<input type="email" name="inputEmail">
+
+ 	<button type="submit">Enviar</button>
+
+ 	<div class="g-recaptcha" data-sitekey="6LfT2yAUAAAAAIj88UBBVBjPrfaobkuMEOKyJM5l"></div>
+ </form>
