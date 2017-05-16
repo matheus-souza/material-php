f1e7e8eb4c9fbad4a9f2854ef17473d42a20fc27
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-05-15 23:40:03 -0300

    Adicionando classe Sql

---
+++ ecommerce/vendor/custom/src/Sql.php
@@ -0,0 +1,45 @@
+<?php 
+	namespace Custom;
+	class Sql{
+		const HOSTNAME = "db_ecommerce";
+		const USERNAME = "root";
+		const PASSWORD = "root";
+		const DBNAME = "db_ecommerce";
+		private $conn;
+		public function __construct()
+		{
+			try {
+				$this->conn = new \PDO(
+					"mysql:dbname=".Sql::DBNAME.";host=".Sql::HOSTNAME,
+					Sql::USERNAME,
+					Sql::PASSWORD
+				);
+			} catch (Exception $e) {
+				throw new PDOException($e);
+			}
+		}
+		private function setParams($statement, $parameters=array())
+		{
+			foreach ($parameters as $key => $value) {
+				$this->bindParam($statement, $key, $value);
+			}
+		}
+		private function bindParam($statement, $key, $value)
+		{
+			$statement->bindParam($key, $value);
+		}
+		public function query($rawQuery, $params = array())
+		{
+			$stmt = $this->conn->prepare($rawQuery);
+			$this->setParams($stmt, $params);
+			$stmt->execute();
+		}
+		public function select($rawQuery, $params = array()):array
+		{
+			$stmt->$this->conn->prepare($rawQuery);
+			$this->setParams($stmt, $params);
+			$stmt->execute();
+			return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+		}
+	}
+?>
