07ea33abfca4ab4e23ce8581276609d6e64f31d1
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-05-17 16:13:41 -0300

    Criando classe para gerenciamento do template

---
+++ ecommerce/vendor/custom/page/Page.php
@@ -0,0 +1,56 @@
+<?php 
+
+	namespace page;
+
+	use Rain\Tpl;
+
+	class Page {
+		private $tpl;
+		private $options = [];
+		private $defaults = [
+			"data"=>[]
+		];
+
+		//Cria o objeto do Rain e monta o header da pagina
+		public function __construct($opts = array()) {
+			$this->options = array_merge($this->defaults, $opts);
+
+			// config
+			$config = array(
+			    "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+			    "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			    "debug"         => true, // set to false to improve the speed
+			);
+
+			Tpl::configure( $config );
+
+			$this->tpl = new Tpl;
+
+			$this->setData($this->options["data"]);
+
+			$this->tpl->draw("header");
+		}
+
+		//Carrega os dados passados para o template
+		private function setData($data = array()) {
+			foreach ($data as $key => $value) {
+				$this->tpl->assign($key, $value);
+			}
+		}
+
+		//Carrega template na tela
+		public function setTpl($name, $data = array(), $returnHtml = false) {
+			$this->setData($data);
+
+			return $this->tpl->draw($name, $returnHtml);
+		}
+
+		//Monta o footer da pagina
+		public function __destruct() {
+			$this->tpl->draw("footer");
+		}
+
+	}
+
+
+ ?>
