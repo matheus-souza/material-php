2d7b3e5a98d5e8efa67436223c17802d67d1b081
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-23 00:45:37 -0300

    Criaando método de deletar usuário

--- ecommerce/vendor/custom/models/User.php
+++ ecommerce/vendor/custom/models/User.php
@@ -96,6 +96,13 @@ class User extends Model {
         $this->setData($results[0]);
     }
 
+    public function delete() {
+        $sql = new Sql();
+
+        $sql->query("CALL sp_users_delete(:iduser)", array(
+            ":iduser" => $this->getiduser()
+        ));
+    }
 }
 
 
