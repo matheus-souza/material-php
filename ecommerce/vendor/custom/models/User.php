1b4c3b34f89725edcac084cafeb27b0b608b1b68
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-23 00:44:54 -0300

    Criando método de buscar usuário

--- ecommerce/vendor/custom/models/User.php
+++ ecommerce/vendor/custom/models/User.php
@@ -69,6 +69,17 @@ class User extends Model {
 
         $this->setData($results[0]);
     }
+
+    public function get($iduser) {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING (idperson) WHERE a.iduser = :iduser", array(
+            ":iduser" => $iduser
+        ));
+
+        $this->setData($results[0]);
+    }
+
 }
 
 
