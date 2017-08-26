2e5d58984537e6867fbc4a086796ae47ed282f6e
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-26 12:07:31 -0300

    Criando rotas para recuperação de senha

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -150,6 +150,33 @@
         $page->setTpl("forgot-sent");
     });
 
+    $app->get('/admin/forgot/reset', function () {
+        $user = User::validForgotDecryp($_GET["code"]);
+
+        $page = new PageAdmin(array("header" => false, "footer" => false));
+
+        $page->setTpl("forgot-reset", array(
+            "name" => $user["desperson"],
+            "code" => $_GET["code"]
+        ));
+    });
+
+    $app->post('/admin/forgot/reset', function () {
+        $forgot = User::validForgotDecryp($_POST["code"]);
+
+        User::setForgotUsed($forgot["idrecovery"]);
+
+        $user = new User();
+
+        $user->get((int)$forgot["iduser"]);
+
+        $user->setPassword($_POST["password"]);
+
+        $page = new PageAdmin(array("header" => false, "footer" => false));
+
+        $page->setTpl("forgot-reset-success");
+    });
+
 	$app->run();
 
  ?>
