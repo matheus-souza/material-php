4a10a146b94880615cfdb3d199164fb5792797df
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-26 03:24:48 -0300

    Criando rotas para envio do e-mail de recuperação de senha

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -137,6 +137,19 @@
         $page->setTpl("forgot");
     });
 
+    $app->post('/admin/forgot', function () {
+        $user = User::getForgot($_POST["email"]);
+
+        header("Location: /admin/forgot/sent");
+        exit();
+    });
+
+    $app->get('/admin/forgot/sent', function () {
+        $page = new PageAdmin(array("header" => false, "footer" => false));
+
+        $page->setTpl("forgot-sent");
+    });
+
 	$app->run();
 
  ?>
