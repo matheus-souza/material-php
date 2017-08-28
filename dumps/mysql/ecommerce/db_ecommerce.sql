62ea69cdb76cd0262db2b6ac1c03de6fd8a23f3d
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-28 18:57:29 -0300

    Adicionando SP de para salvar categoria

--- dumps/mysql/ecommerce/db_ecommerce.sql
+++ dumps/mysql/ecommerce/db_ecommerce.sql
@@ -526,3 +526,26 @@ DELIMITER ;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2017-04-24 11:50:48
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_categories_save` (
+pidcategory INT,
+pdescategory VARCHAR(64)
+)
+BEGIN
+	IF pidcategory > 0 THEN
+
+		UPDATE tb_categories
+        SET descategory = pdescategory
+        WHERE idcategory = pidcategory;
+
+    ELSE
+
+		INSERT INTO tb_categories (descategory) VALUES(pdescategory);
+
+        SET pidcategory = LAST_INSERT_ID();
+
+    END IF;
+
+    SELECT * FROM tb_categories WHERE idcategory = pidcategory;
+END ;;
+DELIMITER ;
