9eebdc34525bc3289008b5e47c02ea349e053b39
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-28 18:54:42 -0300

    Criando model de categoria

---
+++ ecommerce/vendor/custom/models/Category.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace models;
+
+use \Mail\Mailer;
+use \Db\Sql;
+use \Models\Model;
+
+class Category extends Model {
+    public static function listAll() {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
+    }
+
+    public function save() {
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
+            ":idcategory" => $this->getidcategory(),
+            ":descategory" => $this->getdescategory()
+        ));
+
+        $this->setData($results[0]);
+    }
+
+    public function get($iduser) {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING (idperson) WHERE a.iduser = :iduser", array(
+            ":iduser" => $iduser
+        ));
+
+        $this->setData($results[0]);
+    }
+
+    public function update() {
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            ":iduser" => $this->getiduser(),
+            ":desperson" => $this->getdesperson(),
+            ":deslogin" => $this->getdeslogin(),
+            ":despassword" => $this->getdespassword(),
+            ":desemail" => $this->getdesemail(),
+            ":nrphone" => $this->getnrphone(),
+            ":inadmin" => $this->getinadmin()
+        ));
+
+        $this->setData($results[0]);
+    }
+
+    public function delete() {
+        $sql = new Sql();
+
+        $sql->query("CALL sp_users_delete(:iduser)", array(
+            ":iduser" => $this->getiduser()
+        ));
+    }
+}
+?>
