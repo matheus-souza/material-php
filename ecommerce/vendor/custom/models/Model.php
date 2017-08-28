60675f850ef58b0f02e1005973f050b06dc049f5
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-28 18:57:04 -0300

    Corrigindo bug de quando vem nulo

--- ecommerce/vendor/custom/models/Model.php
+++ ecommerce/vendor/custom/models/Model.php
@@ -11,7 +11,7 @@ class Model {
 
         switch ($method) {
             case "get":
-                return $this->values[$fieldName];
+                return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
                 break;
             case "set":
                 $this->values[$fieldName] = $args[0];
