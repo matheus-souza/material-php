92079b66aa9cbfad0b6d02d55ecd573bb6735964
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-30 13:03:30 -0300

    Criado rota para abrir página da categoria

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -254,6 +254,21 @@
         exit();
     });
 
+    $app->get('/categories/{idcategory}', function (Request $request) {
+        $category = new Category();
+
+        $category->get((int)$request->getAttribute('idcategory'));
+
+        $page = new Page();
+
+        $page->setTpl("category", array(
+            "category"=>$category->getValues(),
+            "products"=>array()
+        ));
+
+    });
+
+
 	$app->run();
 
  ?>
