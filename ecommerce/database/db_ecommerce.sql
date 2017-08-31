cde57940c1d9201c080acd3b987ca8456485541c
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-31 12:47:50 -0300

    Criado procedure de save do produto

--- ecommerce/database/db_ecommerce.sql
+++ ecommerce/database/db_ecommerce.sql
@@ -549,3 +549,43 @@ BEGIN
     SELECT * FROM tb_categories WHERE idcategory = pidcategory;
 END ;;
 DELIMITER ;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_products_save`(
+pidproduct int(11),
+pdesproduct varchar(64),
+pvlprice decimal(10,2),
+pvlwidth decimal(10,2),
+pvlheight decimal(10,2),
+pvllength decimal(10,2),
+pvlweight decimal(10,2),
+pdesurl varchar(128)
+)
+BEGIN
+
+	IF pidproduct > 0 THEN
+
+		UPDATE tb_products
+        SET
+			desproduct = pdesproduct,
+            vlprice = pvlprice,
+            vlwidth = pvlwidth,
+            vlheight = pvlheight,
+            vllength = pvllength,
+            vlweight = pvlweight,
+            desurl = pdesurl
+        WHERE idproduct = pidproduct;
+
+    ELSE
+
+		INSERT INTO tb_products (desproduct, vlprice, vlwidth, vlheight, vllength, vlweight, desurl)
+        VALUES(pdesproduct, pvlprice, pvlwidth, pvlheight, pvllength, pvlweight, pdesurl);
+
+        SET pidproduct = LAST_INSERT_ID();
+
+    END IF;
+
+    SELECT * FROM tb_products WHERE idproduct = pidproduct;
+
+END ;;
+DELIMITER ;
