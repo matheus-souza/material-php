55bf93579830f9ff8cea332712753865f06fa474
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-09-04 18:50:35 -0300

    Criando métodos para o tratamento da imagem do produto

--- ecommerce/vendor/custom/models/Product.php
+++ ecommerce/vendor/custom/models/Product.php
@@ -47,5 +47,60 @@ class Product extends Model {
             ":idproduct" => $this->getidproduct()
         ));
     }
+
+    public function checkPhoto() {
+        if (file_exists(
+            '.' .
+            DIRECTORY_SEPARATOR . 'res' .
+            DIRECTORY_SEPARATOR . 'site' .
+            DIRECTORY_SEPARATOR . 'img' .
+            DIRECTORY_SEPARATOR . 'products' .
+            DIRECTORY_SEPARATOR . $this->getidproduct() . '.jpg')) {
+            $url = '../../res/site/img/products/' . $this->getidproduct() . '.jpg';
+        } else {
+            $url =  '../../res/site/img/product.jpg';
+        }
+        return $this->setdesphoto($url);
+    }
+
+    public function getValues()
+    {
+        $this->checkPhoto();
+
+        $values = parent::getValues();
+
+        return $values;
+    }
+
+    public function setPhoto($file) {
+
+        $extension = explode('.', $file['name']);
+        $extension = end($extension);
+
+        switch ($extension) {
+            case 'jpg':case 'jpeg':
+                $image = imagecreatefromjpeg($file['tmp_name']);
+                break;
+            case 'gif':
+                $image = imagecreatefromgif($file['tmp_name']);
+                break;
+            case 'png':
+                $image = imagecreatefrompng($file['tmp_name']);
+                break;
+        }
+
+        $dist = '.' .
+            DIRECTORY_SEPARATOR . 'res' .
+            DIRECTORY_SEPARATOR . 'site' .
+            DIRECTORY_SEPARATOR . 'img' .
+            DIRECTORY_SEPARATOR . 'products' .
+            DIRECTORY_SEPARATOR . $this->getidproduct() . '.jpg';
+        imagejpeg($image, $dist);
+
+        imagedestroy($image);
+
+        $this->checkPhoto();
+    }
+
 }
 ?>
