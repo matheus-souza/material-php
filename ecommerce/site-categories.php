185f223cb5e5f0720aa553e4f42b76f5c3ce0f6f
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-09-18 18:46:08 -0300

    Ajustando rota de paginação

--- ecommerce/site-categories.php
+++ ecommerce/site-categories.php
@@ -6,19 +6,31 @@ use \Models\Category;
 use \Models\Product;
 
 $app->get('/categories/{idcategory}', function (Request $request) {
+    $page = $_GET['page'] ?? 1;
+
     $category = new Category();
 
     $category->get((int)$request->getAttribute('idcategory'));
+
+    $pagination = $category->getProductsPage($page);
+
+    $pages = [];
+
+    for ($i = 1; $i <= $pagination['pages']; $i++) {
+        array_push($pages, [
+            'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+            'page'=>$i
+        ]);
+    }
 
     $page = new Page();
 
     $page->setTpl("category", array(
         "category"=>$category->getValues(),
-        "products"=>Product::checkList($category->getProducts())
+        "products"=>$pagination['data'],
+        "pages"=>$pages
     ));
 
 });
-
-
 
 ?>
