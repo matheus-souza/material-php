54811c3ceb35d8593bc1ae69c2982f940e705d94
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-09-20 13:15:49 -0300

    Criando save do carrinho de compras

--- ecommerce/vendor/custom/models/Cart.php
+++ ecommerce/vendor/custom/models/Cart.php
@@ -10,5 +10,19 @@ use \Models\Product;
 class Cart extends Model {
 
     
+    public function save() {
+        $sql = new Sql();
+
+        $results = $sql->getselect("CALL sp_carts_save(:idcart, :dessessionid, :iduser, :deszipcode, :vlfreight, :nrdays)", [
+            ':idcart'=> $this->getidcart()(),
+            ':dessessionid'=> $this->getdessessionid(),
+            ':iduser'=> $this->getiduser(),
+            ':deszipcode'=> $this->getdeszipcode(),
+            ':vlfreight'=> $this->getvlfreight(),
+            ':nrdays' => $this->getnrdays(),
+        ]);
+
+        $this->setData($results[0]);
+    }
 }
 ?>
