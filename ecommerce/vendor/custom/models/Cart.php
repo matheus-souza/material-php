6b0b06b59bf65c7bc880ae8dfb729ade235bc6c0
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-09-23 11:39:36 -0300

    Criado método para buscar todos os produtos do carrinho

--- ecommerce/vendor/custom/models/Cart.php
+++ ecommerce/vendor/custom/models/Cart.php
@@ -111,5 +111,20 @@ class Cart extends Model {
             ]);
         }
     }
+
+    public function getProducts() {
+        $sql = new Sql();
+
+        return Product::checkList($sql->select("SELECT b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal
+                                        FROM tb_cartsproducts a
+                                  INNER JOIN tb_products b
+                                       USING (idproduct)
+                                       WHERE a.idcart = :idcart
+                                         AND a.dtremoved IS NULL
+                                    GROUP BY b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl
+                                    ORDER BY b.desproduct", [
+            ":idcart" => $this->getidcart()
+        ]));
+    }
 }
 ?>
