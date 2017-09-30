620246482c06a7c0c79beb7f46ce89091e19d6b9
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-09-29 23:06:23 -0300

    Correção de bug

--- ecommerce/functions.php
+++ ecommerce/functions.php
@@ -3,7 +3,9 @@
 use \Models\User;
 use \Db\Sql;
 
-function formatPrice(float $vlprice) {
+function formatPrice($vlprice) {
+    if (!$vlprice > 0) $vlprice = 0;
+
     return number_format($vlprice, 2, ',', '.');
 }
 
