6f664b0c1ff6b253feaf5402360e2d68cce45442
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-03 19:42:13 -0300

    Criando método para consultar no webservice

--- ecommerce/vendor/custom/models/Address.php
+++ ecommerce/vendor/custom/models/Address.php
@@ -8,6 +8,23 @@ use \Models\Model;
 use \Models\Product;
 
 class Address extends Model {
+    public static function getCep($nrcep) {
+        $nrcep = str_replace('-', '', $nrcep);
+        //http://viacep.com.br/ws/01001000/json/
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "http://viacep.com.br/ws/$nrcep/json/");
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $data = json_decode(curl_exec($ch), true);
+
+        curl_close($ch);
+
+        return $data;
+    }
 
 }
 ?>
