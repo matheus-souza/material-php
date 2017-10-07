e3bf1915bbeeb8176fa1c23bebfb77afdb8608fa
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-07 00:38:34 -0300

    Criando rota para geração e exibição do boleto

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -405,6 +405,80 @@
         ]);
     });
 
+    $app->get('/boleto/{idorder}', function (Request $request) {
+
+        User::verifyLogin(false);
+
+        $order = new Order();
+        $order->get((int)$request->getAttribute('idorder'));
+
+
+        // DADOS DO BOLETO PARA O SEU CLIENTE
+        $dias_de_prazo_para_pagamento = 5;
+        $taxa_boleto = 2.95;
+        $data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006";
+        $valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+        $valor_cobrado = str_replace(",", ".",$valor_cobrado);
+        $valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
+
+        $dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
+        $dadosboleto["numero_documento"] = $order->getidorder();	// Num do pedido ou nosso numero
+        $dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+        $dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+        $dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+        $dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da vírgula
+
+        // DADOS DO SEU CLIENTE
+        $dadosboleto["sacado"] = $order->getdesperson();
+        $dadosboleto["endereco1"] = $order->getdesaddress() . ' ' . $order->getdesdistrict();
+        $dadosboleto["endereco2"] = $order->getdescity() . ' - ' . $order->getdesstate() . ' - ' . $order->getdescountry() . ' -  CEP: '. $order->getdeszipcode();
+
+        // INFORMACOES PARA O CLIENTE
+        $dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Nonononono";
+        $dadosboleto["demonstrativo2"] = "Mensalidade referente a nonon nonooon nononon<br>Taxa bancária - R$ ".number_format($taxa_boleto, 2, ',', '');
+        $dadosboleto["demonstrativo3"] = "BoletoPhp - http://www.boletophp.com.br";
+        $dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
+        $dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
+        $dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: xxxx@xxxx.com.br";
+        $dadosboleto["instrucoes4"] = "- Emitido pelo sistema Projeto BoletoPhp - www.boletophp.com.br";
+
+        // DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+        $dadosboleto["quantidade"] = "";
+        $dadosboleto["valor_unitario"] = "";
+        $dadosboleto["aceite"] = "";
+        $dadosboleto["especie"] = "R$";
+        $dadosboleto["especie_doc"] = "";
+
+
+        // ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+
+
+        // DADOS DA SUA CONTA - ITAÚ
+        $dadosboleto["agencia"] = "1565"; // Num da agencia, sem digito
+        $dadosboleto["conta"] = "13877";	// Num da conta, sem digito
+        $dadosboleto["conta_dv"] = "4"; 	// Digito do Num da conta
+
+        // DADOS PERSONALIZADOS - ITAÚ
+        $dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+
+        // SEUS DADOS
+        $dadosboleto["identificacao"] = "BoletoPhp - Código Aberto de Sistema de Boletos";
+        $dadosboleto["cpf_cnpj"] = "";
+        $dadosboleto["endereco"] = "Coloque o endereço da sua empresa aqui";
+        $dadosboleto["cidade_uf"] = "Cidade / Estado";
+        $dadosboleto["cedente"] = "Coloque a Razão Social da sua empresa aqui";
+
+        // NÃO ALTERAR!
+        $path = $_SERVER['DOCUMENT_ROOT'] .
+                DIRECTORY_SEPARATOR .
+                'res' . DIRECTORY_SEPARATOR .
+                'boletophp' . DIRECTORY_SEPARATOR .
+                'include' . DIRECTORY_SEPARATOR;
+
+        require_once ($path . 'funcoes_itau.php');
+        require_once ($path . 'layout_itau.php');
+    });
+
 $app->run();
 
  ?>
