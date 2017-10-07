374c6bc8c8013887c5d91a2edf672a67c50933ca
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-06 23:37:38 -0300

    Criado método de save

--- ecommerce/vendor/custom/models/Order.php
+++ ecommerce/vendor/custom/models/Order.php
@@ -2,12 +2,24 @@
 
 namespace models;
 
-use \Mail\Mailer;
 use \Db\Sql;
-use \Models\Model;
-use \Models\Product;
 
 class Order extends Model {
+    public function save() {
+        $sql = new Sql();
 
+        $results = $sql->select("CALL sp_orders_save(:idorder, :idcart, :iduser, :idstatus, :idaddress, :vltotal)", [
+            ':idorder' => $this->getidorder(),
+            ':idcart' => $this->getidcart(),
+            ':iduser' => $this->getiduser(),
+            ':idstatus' => $this->getidstatus(),
+            ':idaddress' => $this->getidaddress(),
+            ':vltotal' => $this->getvltotal()
+        ]);
+
+        if (count($results) > 0) {
+            $this->setData($results[0]);
+        }
+    }
 }
 ?>
