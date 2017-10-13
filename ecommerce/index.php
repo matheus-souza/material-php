b72b2688464880de8a84d06c9a7a3437f12fdfa3
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-13 12:40:20 -0300

    Adicionando require para arvo de rotas dos pedidos

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -24,6 +24,7 @@
     require_once "admin-login.php";
     require_once "admin-categories.php";
     require_once "admin-products.php";
+    require_once "admin-orders.php";
     require_once "functions.php";
 
     $app->get('/', function () {
