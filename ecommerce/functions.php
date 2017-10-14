2bf7ab5f4044abb657c3975ff89c0550878df5a0
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-14 09:50:19 -0300

    Criada função para formatar data

--- ecommerce/functions.php
+++ ecommerce/functions.php
@@ -10,6 +10,10 @@ function formatPrice($vlprice) {
     return number_format($vlprice, 2, ',', '.');
 }
 
+function formatDate($date) {
+    return date('d/m/Y', strtotime($date));
+}
+
 function checkLogin($inadmin = true)
 {
     return User::checkLogin($inadmin);
