234f0c19973558ed2dc5fd537dc1666f75d5a9ea
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-14 18:28:41 -0300

    Ajustando rota da pesquisa

--- ecommerce/admin-categories.php
+++ ecommerce/admin-categories.php
@@ -18,7 +18,7 @@ $app->get('/admin/categories', function () {
 
     for ($x = 0; $x < $pagination['pages']; $x++) {
         array_push($pages, [
-            'href' => '/admin/users?'.http_build_query([
+            'href' => '/admin/categories?'.http_build_query([
                     'page' => $x+1,
                     'search' => $search
                 ]),
