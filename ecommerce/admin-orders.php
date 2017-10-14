1540f8ad9e50962f2d63ac960c594374d78050e3
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-14 11:08:39 -0300

    Cirado rotas para editar status do pedido

--- ecommerce/admin-orders.php
+++ ecommerce/admin-orders.php
@@ -8,6 +8,49 @@ use \Models\Product;
 use \Slim\Http\Request;
 
 
+$app->post('/admin/orders/{idorder}/status', function (Request $request) {
+    User::verifyLogin();
+
+    $idOrder = (int)$request->getAttribute('idorder');
+
+    if (!isset($_POST['idstatus']) || !(int)$_POST['idstatus'] > 0) {
+        Order::setError("Informe o status atual.");
+
+        header("Location: /admin/orders/".$idOrder."/status");
+        exit();
+    }
+
+    $order = new Order();
+
+    $order->get($idOrder);
+
+    $order->setidstatus((int)$_POST['idstatus']);
+
+    $order->save();
+
+    Order::setSuccess("Status atualizado");
+
+    header("Location: /admin/orders/{$idOrder}/status");
+    exit();
+});
+
+$app->get('/admin/orders/{idorder}/status', function (Request $request) {
+    User::verifyLogin();
+
+    $order = new Order();
+
+    $order->get((int)$request->getAttribute('idorder'));
+
+    $page = new PageAdmin();
+
+    $page->setTpl("order-status", [
+        'order' => $order->getValues(),
+        'status' => OrderStatus::listAll(),
+        'msgSuccess' => Order::getSuccess(),
+        'msgError' => Order::getError(),
+    ]);
+});
+
 $app->get('/admin/orders/{idorder}', function (Request $request) {
     User::verifyLogin();
 
