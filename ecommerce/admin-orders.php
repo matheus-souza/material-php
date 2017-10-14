69cb6fb61725467c0d1e09b2ce5ea0dc33ab1239
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-14 10:54:44 -0300

    Criada rota para detalhes do pedido

--- ecommerce/admin-orders.php
+++ ecommerce/admin-orders.php
@@ -3,8 +3,28 @@
 use \Page\PageAdmin;
 use \Models\User;
 use \Models\Order;
+use \Models\OrderStatus;
 use \Models\Product;
 use \Slim\Http\Request;
+
+
+$app->get('/admin/orders/{idorder}', function (Request $request) {
+    User::verifyLogin();
+
+    $order = new Order();
+
+    $order->get((int)$request->getAttribute('idorder'));
+
+    $cart = $order->getCart();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("order", [
+        'order' => $order->getValues(),
+        'cart' => $cart->getValues(),
+        'products' => $cart->getProducts()
+    ]);
+});
 
 $app->get('/admin/orders', function () {
     User::verifyLogin();
