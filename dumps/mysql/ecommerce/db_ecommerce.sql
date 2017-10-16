717db03676ed07809fd462699e3aba855038d1aa
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-16 12:23:32 -0200

    Ajustes na base de dados
    Como a procedure  de deletar usuários e a tabela de usuarios foi criada a bastante tempo precisamos dar manutenção nas mesmas

--- dumps/mysql/ecommerce/db_ecommerce.sql
+++ dumps/mysql/ecommerce/db_ecommerce.sql
@@ -449,16 +449,27 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_users_delete`(
 piduser INT
 )
 BEGIN
-	
+
     DECLARE vidperson INT;
-    
+
+    SET FOREIGN_KEY_CHECKS = 0;
+
 	SELECT idperson INTO vidperson
     FROM tb_users
     WHERE iduser = piduser;
 
+    DELETE FROM tb_addresses WHERE idperson = vidperson;
+    DELETE FROM tb_addresses WHERE idaddress IN(SELECT idaddress FROM tb_orders WHERE iduser = piduser);
+	  DELETE FROM tb_persons WHERE idperson = vidperson;
+
+    DELETE FROM tb_userslogs WHERE iduser = piduser;
     DELETE FROM tb_userspasswordsrecoveries WHERE iduser = piduser;
+    DELETE FROM tb_orders WHERE iduser = piduser;
+    DELETE FROM tb_cartsproducts WHERE idcart IN(SELECT idcart FROM tb_carts WHERE iduser = piduser);
+    DELETE FROM tb_carts WHERE iduser = piduser;
     DELETE FROM tb_users WHERE iduser = piduser;
-    DELETE FROM tb_persons WHERE idperson = vidperson;
+
+    SET FOREIGN_KEY_CHECKS = 1;
     
 END ;;
 DELIMITER ;
@@ -485,7 +496,7 @@ pnrphone BIGINT,
 pinadmin TINYINT
 )
 BEGIN
-	
+
     DECLARE vidperson INT;
     
 	INSERT INTO tb_persons (desperson, desemail, nrphone)
