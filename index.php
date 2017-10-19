3613c754eae9cf0414c176fa09f57afd83809173
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-10-19 19:10:49 -0200

    Ajustando index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php 
-	echo "Hello world";
- ?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+<div>
+    <div class="row">
+        <div class="col-sm">
+        </div>
+        <div class="col-sm">
+            <h1>Material sobre PHP</h1>
+        </div>
+        <div class="col-sm">
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+        </div>
+        <div class="col-sm">
+            <a class="btn btn-primary" href="/exemplos" role="button">Exemplos de PHP</a>
+            <a class="btn btn-primary" href="localhost:8000" role="button">Ecommerce</a>
+        </div>
+        <div class="col-sm">
+        </div>
+    </div>
+</div>
